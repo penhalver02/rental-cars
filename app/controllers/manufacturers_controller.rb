759ce88514509f6ed19b05619ba624aeb60ee9b6
Lucas <penhalver.lucas@gmail.com>
2020-04-07 18:05:51 -0300

    Admin register funcionando

---
+++ app/controllers/manufacturers_controller.rb
@@ -0,0 +1,31 @@
+class ManufacturersController < ApplicationController
+  
+  def index
+    @manufacturers = Manufacturer.all
+    #binding.pry
+  end
+
+  def show
+    @manufacturer = Manufacturer.find(params[:id])
+  end
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+
+    if @manufacturer.save
+      redirect_to @manufacturer
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name)
+  end
+end
