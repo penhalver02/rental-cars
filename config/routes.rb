3ad48ec325dcd6d317ea2604c8dd2c39f3cdad55
Lucas Penhalver <penhalver.lucas@gmail.com>
2020-04-13 18:45:07 -0300

    rotas arrumadas

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
   resources :manufacturers, only: [:show, :index, :create, :new]
-  resources :subsidiaries
-  resources :car_categories
+  resources :subsidiaries, only: [:show, :index, :create, :new, :edit, :update]
+  resources :car_categories, only: [:show, :index, :create, :new]
   resources :customers,  only: [:show, :index, :create, :new]
 end
