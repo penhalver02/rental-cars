aca7d56ea7ac068ba6bb504b7df2cf9ad648c6b3
Lucas Penhalver <penhalver.lucas@gmail.com>
2020-04-18 16:04:28 -0300

    Delete de fabricante

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
-  resources :manufacturers, only: [:show, :index, :create, :new]
+  resources :manufacturers, only: [:show, :index, :create, :new, :destroy]
   resources :subsidiaries, only: [:show, :index, :create, :new, :edit, :update]
   resources :car_categories, only: [:show, :index, :create, :new]
   resources :customers,  only: [:show, :index, :create, :new]
