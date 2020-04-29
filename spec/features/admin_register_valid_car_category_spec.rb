a7abc8d0c1a2f45aea2aea2baaf355ed735890c8
Lucas Penhalver <penhalver.lucas@gmail.com>
2020-04-29 00:40:16 -0300

    car categori translated

--- spec/features/admin_register_valid_car_category_spec.rb
+++ spec/features/admin_register_valid_car_category_spec.rb
@@ -1,23 +1,6 @@
 require 'rails_helper'
 
 feature 'Admin register valid car category' do
-  scenario 'and name must be unique' do
-    CarCategory.create(name: 'A', daily_rate: 100,car_insurance: 100, third_part_insurance: 50)
-    
-    visit root_path
-    click_on 'Categorias de carro'
-    click_on 'Registrar nova categoria'
-
-   
-    fill_in 'Categoria', with: 'A'
-    fill_in 'Preço da diaria', with: 100
-    fill_in 'Preço do seguro', with: 100
-    fill_in 'Preço do seguro para terceiro', with: 50
-    click_on 'Enviar'
-
-   expect(page).to have_content('Nome deve ser único')
-  end
-
   scenario 'and attribute can not be blank' do
     visit root_path
     click_on 'Categorias de carro'
@@ -31,6 +14,9 @@ feature 'Admin register valid car category' do
     fill_in 'Preço do seguro para terceiro', with: ''
     click_on 'Enviar'
 
-    expect(page).to have_content('Todos atributos não podem ficar em branco')
+    expect(page).to have_content('Categoria não pode ficar em branco')
+    expect(page).to have_content('Preço da diaria não pode ficar em branco')
+    expect(page).to have_content('Preço do seguro não pode ficar em branco')
+    expect(page).to have_content('Preço do seguro para terceiro não pode ficar em branco')
   end
 end
--- spec/models/car_category_spec.rb
+++ spec/models/car_category_spec.rb
@@ -2,24 +2,36 @@ require 'rails_helper'
 
 RSpec.describe CarCategory, type: :model do
   context 'validation' do
-    it 'attribute cannot be blank' do
+    it 'name cannot be blank' do
       car_category = CarCategory.new
 
       car_category.valid?
 
-      expect(car_category.errors[:name]).to include('Todos atributos não podem ficar em branco')
-      expect(car_category.errors[:daily_rate]).to include('Todos atributos não podem ficar em branco')
-      expect(car_category.errors[:car_insurance]).to include('Todos atributos não podem ficar em branco')
-      expect(car_category.errors[:third_part_insurance]).to include('Todos atributos não podem ficar em branco')
+      expect(car_category.errors[:name]).to include('não pode ficar em branco')
     end
 
-    it 'name must be uniq' do
-      CarCategory.create!(name: 'Hatch', daily_rate: 5.1,car_insurance: 300, third_part_insurance: 100)
-      car_category = CarCategory.new(name: 'Hatch', daily_rate: 5.1,car_insurance: 300, third_part_insurance: 100)
+    it 'daily rate cannot be blank' do
+      car_category = CarCategory.new
 
       car_category.valid?
 
-      expect(car_category.errors[:name]).to include('Nome deve ser único')
+      expect(car_category.errors[:daily_rate]).to include('não pode ficar em branco')
+    end
+
+    it 'car insurance cannot be blank' do
+      car_category = CarCategory.new
+
+      car_category.valid?
+
+      expect(car_category.errors[:car_insurance]).to include('não pode ficar em branco')
+    end
+
+    it 'third part insurance cannot be blank' do
+      car_category = CarCategory.new
+
+      car_category.valid?
+
+      expect(car_category.errors[:third_part_insurance]).to include('não pode ficar em branco')
     end
   end
 end
