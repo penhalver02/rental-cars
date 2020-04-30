d085406912fa747758af776622d18e4af91be6a3
Lucas Penhalver <penhalver.lucas@gmail.com>
2020-04-30 17:21:59 -0300

    login and logout

---
+++ spec/features/user_authentication_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+feature 'User authentication' do
+  context 'log in' do
+    scenario 'successfully' do
+      user = User.create!(email: 'lucas@gmail.com', password: '12345678')
+
+      visit root_path
+      click_on 'Entrar'
+      fill_in 'Email', with: user.email
+      fill_in 'Senha', with: user.password
+      within 'form' do
+        click_on 'Entrar'
+      end
+      
+      
+      expect(page).to have_content('Login efetuado com sucesso')
+      expect(page).not_to have_link('Entrar')
+      expect(page).to have_link('Sair')
+      expect(current_path).to eq(root_path)
+    end
+
+    xscenario 'and must fill in all fields' do
+    end
+  end
+
+  context 'log out' do
+    scenario 'successfully' do
+      user = User.create!(email: 'lucas@gmail.com', password: '12345678')
+
+      visit root_path
+      click_on 'Entrar'
+      fill_in 'Email', with: user.email
+      fill_in 'Senha', with: user.password
+      within 'form' do
+        click_on 'Entrar'
+      end
+      click_on 'Sair'
+      
+      
+      expect(page).to have_content('Saiu com sucesso')
+      expect(page).to have_link('Entrar')
+      expect(page).not_to have_link('Sair')
+      expect(current_path).to eq(root_path)
+    end
+  end
+end
